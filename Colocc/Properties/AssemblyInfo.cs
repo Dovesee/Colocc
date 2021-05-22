0d37ca5e3d47998ed7a9bf940534dc1d6fd227da
shi <doveseeyou@hotmail.com>
2021-05-22 15:57:43 +0800

    忽略

--- Colocc/Properties/AssemblyInfo.cs
+++ Colocc/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ internal static class ColoccAssembly
 { 
     public const string Title = "Colocc"; 
     public const string Version = "1.0.0." + GitVersionCount; 
-    public const string BuildTime = "2021/05/22 15:50"; 
-    public const string GitVersionCommit = "091c6be"; 
-    private const string GitVersionCount = "6"; 
+    public const string BuildTime = "2021/05/22 15:56"; 
+    public const string GitVersionCommit = "faf3324"; 
+    private const string GitVersionCount = "7"; 
 } 
